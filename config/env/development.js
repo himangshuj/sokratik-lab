3be89165b34a8f28ce4e889f024b381a25b60211
himangshu <himangshu@sokratik.com>
2013-12-27 14:18:56 +0530

    [configs] mean.io now works on my laptop
    Signed-off-by: himangshu <himangshu@sokratik.com>

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: "mongodb://localhost/mean-dev",
+    db: "mongodb://sokratik-trial:sokratik-trial@localhost:10000/sokratik-trial",
     app: {
         name: "MEAN - A Modern Stack - Development"
     },
