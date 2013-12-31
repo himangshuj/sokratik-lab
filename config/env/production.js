9d81d5fb66fa0d11e4d8be8d8c9a564504c3333b
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2014-01-01 02:48:39 +0530

    [navigation] resolve audioLocation step-by-step from s3

--- config/env/production.js
+++ config/env/production.js
@@ -24,5 +24,11 @@ module.exports = {
         clientID: "APP_ID",
         clientSecret: "APP_SECRET",
         callbackURL: "http://localhost:3000/auth/google/callback"
+    },
+    s3:{
+        ACCESS_KEY: "AKIAJ6Q7O3H2BK6ZGNGA",
+        SECRET: "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",
+        BUCKET: "sokratik-audio",
+        AUDIOLOCATION_PREFIX: "http://sokratik-audio.s3.amazonaws.com/"
     }
 }
