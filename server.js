6113d80f92140fc60ffabefbddd33911c72041b8
himangshu <himangshu@sokratik.com>
2014-01-05 11:02:16 +0530

    [v1.0] fixing db configs for server.js
    Signed-off-by: himangshu <himangshu@sokratik.com>

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var config = require('./config/config'),
     mongoose = require('mongoose');
 
 //Bootstrap db connection
-var db = mongoose.connect(config.db);
+var db = mongoose.connect(config.db.uri, config.db.options);
 
 //Bootstrap models
 //Bootstrap models
