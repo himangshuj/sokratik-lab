19b3cba29f99dc7ea8be041c3ec13cbb3fff93df
himangshu <himangshu@sokratik.com>
2014-01-16 16:46:59 +0530

    [share-presentations] Ability to share on google+
    Signed-off-by: himangshu <himangshu@sokratik.com>

--- assets/js/app/share/share.js
+++ assets/js/app/share/share.js
@@ -34,18 +34,23 @@
     /**
      * And of course we define a controller for our route.
      */
-        .controller('ShareCtrl', ['$rootScope', 'presentation', '$scope', function ($rootScope, presentation, $scope) {
+        .controller('ShareCtrl', ['$rootScope', 'presentation', '$scope', '$window', function ($rootScope, presentation, $scope, $window) {
             $rootScope.presentationMode = false;
             $rootScope.navigationMode = true;
             $rootScope.homeScreen = true;
             $scope.title = presentation.presentationData[0].keyVals.title;
             $scope.presentationId = presentation._id;
-            var playUrl = "http://lab.sokratik.com/#/play/" + presentation._id + "/0//0/init";
+            var playUrl = "http://lab.sokratik.com/play/" + presentation._id;
+            $scope.directPlayUrl = playUrl;
             $scope.iframeLink = "<iframe width=\"420\" height=\"315\" " +
-                " src=\""+playUrl+"\" frameborder=\"0\"  allowfullscreen> </iframe>"
+                " src=\"" + playUrl + "\" frameborder=\"0\"  allowfullscreen> </iframe>"
             $scope.fbShareLink = "https://www.facebook.com/dialog/feed?app_id=412707485526849" +
-                "&display=popup&caption=" + encodeURIComponent($scope.title)+
-                "&link="+encodeURIComponent(playUrl)+
+                "&display=popup&caption=" + encodeURIComponent($scope.title) +
+                "&link=" + encodeURIComponent(playUrl) +
                 "&redirect_uri=http://lab.sokratik.com";
+            $scope.shareGooglePlus = function () {
+                var url = "https://plus.google.com/share?url=" + playUrl;
+                $window.open(url,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+            }
         }]);
 })(angular, 'sokratik.lab.share');
